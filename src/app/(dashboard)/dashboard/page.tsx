40bc9dc4b7db0d7be1479d1a8d5fc703fa517817
jahidpolygontech <jahid.iqbal@polygontech.xyz>
2025-07-09 18:17:38 +0600

    ➕ Add design of dashboard
    Type(s): ADD
    
    Issue(s):
    - JIRA: nia

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,26 +1,38 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 const Page = () => {
   return (
     <div className="w-full">
       <div className="flex justify-between items-center px-4 py-3">
-        {/* Breadcrumb */}
+
         <div className="flex items-center gap-3 cursor-pointer">
           <span className="text-primary font-semibold">Shipments</span>
-          <Image src="/Images/dashboard/arrow-right.svg" alt="arrow" width={16} height={16} />
-          <span className="text-gray-500 ">Create</span>
+          <Image
+            src="/Images/dashboard/arrow-right.svg"
+            alt="arrow"
+            className="w-4 h-4"
+            height={20}
+            width={20}
+          />
+          <span className="text-gray-500">Create</span>
         </div>
 
         {/* Search bar */}
-        <div className="relative">
+        <div className="relative w-full max-w-sm">
           <input
             type="text"
             placeholder="Search with Con-note ID/ Reference ID..."
-            className="border border-gray-300 text-gray-600 rounded-md pl-10 pr-4 py-2 focus:outline-none focus:ring-1 focus:ring-primary"
+            className="w-full border border-gray-300 rounded-md text-gray-500 text-sm pl-10 pr-3 py-2 focus:outline-none focus:ring-1 focus:ring-primary"
           />
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-            <Image src="/Images/dashboard/search-normal.svg" alt="search" width={20} height={20} />
+            <Image
+              src="/Images/dashboard/search-normal.svg"
+              alt="search"
+              className="w-5 h-5"
+              height={20}
+              width={20}
+            />
           </div>
         </div>
       </div>
